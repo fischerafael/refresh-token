f37a75cf0cac1b7461c68053b7927c1e5fceaee5
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-03-02 20:13:40 -0300

    feat: created cookie class

---
+++ src/utils/cookie.ts
@@ -0,0 +1,21 @@
+import cookie from "js-cookie";
+
+export class Cookie {
+  constructor(private key: string = "@test") {}
+
+  set(value: any) {
+    cookie.set(this.key, JSON.stringify(value));
+  }
+
+  get() {
+    try {
+      return cookie.get(this.key);
+    } catch (e) {
+      return undefined;
+    }
+  }
+
+  delete() {
+    cookie.remove(this.key);
+  }
+}
