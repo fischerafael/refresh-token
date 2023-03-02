36b150bf9071d509ad83b2b959056e158dce68c6
Rafael Fischer <rafaelsanfischer@gmail.com>
2023-03-02 20:09:05 -0300

    feat: creating login

---
+++ src/pages/PageLogin/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const PageLogin = () => {
+  return (
+    <div>
+      <input placeholder="Email" type="email" />
+      <input placeholder="password" type="password" />
+      <button>Log In</button>
+    </div>
+  );
+};
